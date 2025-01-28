5810ba5653215a151e203e02ca646f8ce3014d84
hillphang <26hillary.phang@lwhs.org>
2025-01-28 23:21:30 +0000

    submitting my first code

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,19 @@
+let myMessage = "good morning"
+let xLocation = 30; 
+function preload(){
+  myFont = loadFont("Antarlika.otf")
+}
 function setup() {
   createCanvas(400, 400);
-}
+   background(50);
+  noStroke();
+  fill(255);
+  textFont(myFont)
 
+  text(myMessage, 30, 200, 250,200);
+  xLocation = xLocation +1;
+ 
+}
 function draw() {
-  background(200);
-  
+ 
 }
