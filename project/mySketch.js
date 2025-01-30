c211ef2648a32e217ca248ca80c8a9fffce491dc
hillphang <26hillary.phang@lwhs.org>
2025-01-30 22:06:48 +0000

    Submitting Second Draft

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,19 +1,29 @@
-let myMessage = "good morning"
-let xLocation = 30; 
+let myMessage = "left arrow ~ sunset, right arrow ~ sunrise"
+
 function preload(){
   myFont = loadFont("Antarlika.otf")
 }
 function setup() {
-  createCanvas(400, 400);
-   background(50);
+  createCanvas(570, 525);
+  background(25,25,112);
   noStroke();
   fill(255);
   textFont(myFont)
-
-  text(myMessage, 30, 200, 250,200);
-  xLocation = xLocation +1;
+  textSize(40)
+  text(myMessage, 30, 50);
+  
  
 }
-function draw() {
- 
+function draw() { 
+}
+
+function keyPressed(){
+  if(keyCode === LEFT_ARROW){
+  fill(102,153,204)
+  }  
+  if(keyCode === RIGHT_ARROW){
+  fill(255, 253, 208)
+  }  
+  
+  ellipse(random(0,525),random(0,525),500,20) 
 }
