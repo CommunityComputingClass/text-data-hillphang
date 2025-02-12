2d4b9c316ea826308154bb879fbd342538d1ec31
hillphang <26hillary.phang@lwhs.org>
2025-02-12 21:28:01 +0000

    Survey Code

--- project/myForum.js
+++ project/myForum.js
@@ -1,4 +1,9 @@
-let myQuestion = "What do you do to lift your mood when you're feeling low?"
+let myMessage = "left arrow ~ sunset, right arrow ~ sunrise"
+
+function preload(){
+  myFont = loadFont("Antarlika.otf")
+}
+let myQuestion = "If you were a season, what season would you be?"
  
 let contents = "";
 
@@ -10,9 +15,10 @@ function setup() {
   Button.position(220,80)
   Button.mousePressed(showText)
   
-  background(50);
-  fill(255);
-  textSize(14);
+  background(230, 242, 255);
+  fill("navy");
+  textSize(30);
+  textFont(myFont)
   text(myQuestion,50,50)  
 }
 
